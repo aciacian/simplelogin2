61e1dc9f42575516398a78c09ed974ed25970183
aciacian <astri219@gmail.com>
2022-11-03 10:23:57 +0700

    5

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const {Strategy} = require("passport-google-oauth2");
 passport.use(new Strategy({
     clientID:"363824345011-49gjo9sjuekpt59t27o0208tlq97u6bq.apps.googleusercontent.com",
     clientSecret:"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",
-    callbackURL:"http://localhost:8000/api/v1/auth/google",
+    callbackURL:"https://aciaciandeploy.herokuapp.com/api/v1/auth/google",
     passReqToCallback:true
 },(request, accessToken, refreshToken, profile, done)=> {
     return done(null, profile)
